caeafe19ed57707c5f6a80307993d53e66bf3342
vfs <vfs@wegbuxen.biz>
2021-02-14 10:01:48 +0100

    .

--- Logic/Bot.cs
+++ Logic/Bot.cs
@@ -1,8 +1,11 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -17,6 +20,7 @@ namespace unbis_discord_bot
         public static DiscordClient Client { get; private set; }
         public InteractivityExtension Interactivity { get; private set; }
         public CommandsNextExtension Commands { get; private set; }
+        public readonly EventId BotEventId = new EventId(42, "exekutivfs");
 
         public static ConfigJson configJson { get; set; }
         public async Task TaskAsync()
@@ -73,6 +77,11 @@ namespace unbis_discord_bot
             Commands.RegisterCommands<SimpleCmds>();
             Commands.RegisterCommands<Urls>();
             Commands.RegisterCommands<Wegbuxen>();
+            Commands.CommandExecuted += Commands_CommandExecuted;
+            Commands.CommandErrored += Commands_CommandErrored;
+
+            Client.ClientErrored += Client_ClientError;
+            Client.GuildAvailable += Client_GuildAvailable;
 
             await Client.ConnectAsync();
 
@@ -81,6 +90,41 @@ namespace unbis_discord_bot
 
         private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
         {
+            sender.Logger.LogInformation(BotEventId, "Client läuft");
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, $"Guild verbunden: {e.Guild.Name}");
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            e.Context.Client.Logger.LogInformation(BotEventId, $"{e.Context.User.Username} erfolgreich ausgeführt'{e.Command.QualifiedName}'");
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandErrored(CommandsNextExtension sender, CommandErrorEventArgs e)
+        {
+            e.Context.Client.Logger.LogError(BotEventId, $"{e.Context.User.Username} versuchte '{e.Command?.QualifiedName ?? "<unbekannter befehl>"}' Fehler: {e.Exception.GetType()}: {e.Exception.Message ?? "<keine nachricht>"}", DateTime.Now);
+            if (e.Exception is ChecksFailedException ex)
+            {
+                var emoji = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emoji} Keine ausreichenden Rechte",
+                    Color = new DiscordColor(0xFF0000)
+                };
+                await e.Context.RespondAsync("", embed: embed);
+            }
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            sender.Logger.LogError(e.Exception, "Exception aufgetreten");
             return Task.CompletedTask;
         }
     }
