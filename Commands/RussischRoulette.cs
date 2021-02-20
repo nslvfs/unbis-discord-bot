123a1998f52ba8d71e04cdefae7c0991bd753ed1
vfs <vfs@wegbuxen.biz>
2021-02-20 21:03:10 +0100

    .

--- Commands/RussischRoulette.cs
+++ Commands/RussischRoulette.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,52 +11,76 @@ namespace unbis_discord_bot.Commands
     {
         private static List<Model.PerChannelRoulette> rouletteData = new List<Model.PerChannelRoulette>();
 
+        SemaphoreSlim _semaphoregate = new SemaphoreSlim(1,1);
+
         [Command("roulette")]
         [Aliases("r")]
         [Description("Russischer Familienspaß")]
         public async Task Roulette(CommandContext ctx)
         {
-            var found = false;
-            foreach (var item in rouletteData)
-            {
-                if (item.channelId == ctx.Channel.Id)
+            try {
+                int index = 0;
+                await _semaphoregate.WaitAsync();
+                var found = false;
+                foreach (var item in rouletteData)
                 {
-                    found = true;
-                    if (item.revKammer == -1)
+                    if (item.channelId == ctx.Channel.Id)
                     {
-                        var text = DSharpPlus.Formatter.Italic("lädt den Revolver");
-                        await ctx.Channel.SendMessageAsync(text).ConfigureAwait(false);
-                        item.revKammer = Shared.GenerateRandomNumber(1, 8);
-                        item.revShots = 0;
+                        found = true;
+                        break;
                     }
-                    item.revShots++;
-                    if (item.revShots != item.revKammer)
+                    index++;
+                }
+                if (!found)
+                {
+                    Model.PerChannelRoulette newItem = new Model.PerChannelRoulette();
+                    newItem.channelId = ctx.Channel.Id;
+                    newItem.revShots = -1;
+                    rouletteData.Add(newItem);
+                }
+                await RouletteLogic(ctx, rouletteData.Count - 1);
+                _semaphoregate.Release();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static async Task RouletteLogic(CommandContext ctx, int index)
+        {
+
+
+            if (rouletteData[index].channelId == ctx.Channel.Id)
+            {
+                if (rouletteData[index].revKammer == -1)
+                {
+                    var text = DSharpPlus.Formatter.Italic("lädt den Revolver");
+                    await ctx.Channel.SendMessageAsync(text).ConfigureAwait(false);
+                    rouletteData[index].revKammer = Shared.GenerateRandomNumber(1, 8);
+                    rouletteData[index].revShots = 0;
+                }
+                rouletteData[index].revShots++;
+                if (rouletteData[index].revShots != rouletteData[index].revKammer)
+                {
+
+                    await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": ** klick **").ConfigureAwait(false);
+                }
+                if (rouletteData[index].revShots == rouletteData[index].revKammer)
+                {
+                    rouletteData[index].revKammer = -1;
+                    await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": ** BOOM **").ConfigureAwait(false);
+                    if (ctx.Guild.Id == 791393115097137171)
                     {
-                        await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": ** klick **").ConfigureAwait(false);
+                        var roleMuted = ctx.Guild.GetRole(807921762570469386);
+                        await ctx.Member.GrantRoleAsync(roleMuted);
+                        Bot.RemoveUserfromMessageArchiv(ctx.Member.Id);
+                        Thread.Sleep(1000 * 60 * 10); // 10 Min
+                        await ctx.Member.RevokeRoleAsync(roleMuted);
+                        await ctx.Channel.SendMessageAsync(ctx.Member.Mention + " jetzt nicht mehr gemuted").ConfigureAwait(false);
                     }
-                    if (item.revShots == item.revKammer)
-                    {
-                        item.revKammer = -1;
-                        await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": ** BOOM **").ConfigureAwait(false);
-                        if (ctx.Guild.Id == 791393115097137171) { 
-                            var roleMuted = ctx.Guild.GetRole(807921762570469386);
-                            await ctx.Member.GrantRoleAsync(roleMuted);
-                            Bot.RemoveUserfromMessageArchiv(ctx.Member.Id);
-                            Thread.Sleep(1000 * 60 * 10); // 10 Min
-                            await ctx.Member.RevokeRoleAsync(roleMuted);
-                            await ctx.Channel.SendMessageAsync(ctx.Member.Mention + " jetzt nicht mehr gemuted").ConfigureAwait(false);
-                        }
-                    }
-                    break;
                 }
             }
-            if (!found)
-            {
-                Model.PerChannelRoulette newItem = new Model.PerChannelRoulette();
-                newItem.channelId = ctx.Channel.Id;
-                rouletteData.Add(newItem);
-                await Roulette(ctx);
-            }
+            
 
         }
     }
