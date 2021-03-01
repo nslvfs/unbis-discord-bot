5e1e33569f84b24cdea45885ef2743430e544279
vfs <vfs@wegbuxen.biz>
2021-03-01 15:29:45 +0100

    .

--- Logic/Bot.cs
+++ Logic/Bot.cs
@@ -314,7 +314,7 @@ namespace unbis_discord_bot
                 "🇰🇦", "🇿🇪", "𝖟𝖊", "|<atze", "|</-\\tze", "qadse", "quadse", "koschka", "kxaxtxzxe", "|<atze", "k4tze",
                 "katže", "kazze", "kätz", "mieds", "kattze", "mîez", "mîezekåtze", "kitten", "kity", "kitties", "gatto",
                 ":regional_indicator_k: :regional_indicator_a: :regional_indicator_t: :regional_indicator_z: :regional_indicator_e:",
-                "kudze", "kaatzee", "kazeh", "|<atz", "🐈", "gartzen", "gardsen", "cutsen", "kaaatzeeeen", "ќáꮀźëň", "schnoppo", "cät" };
+                "kudze", "kaatzee", "kazeh", "|<atz", "🐈", "gartzen", "gardsen", "cutsen", "kaaatzeeeen", "ќáꮀźëň", "schnoppo", "cät", "Кот" };
             foreach (var item in badWords)
             {
                 if (Message.Contains(item))
