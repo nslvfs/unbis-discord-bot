5240e3c57bb83966a6d62970633834d68deef581
vfs <vfs@wegbuxen.biz>
2021-10-02 09:14:19 +0200

    .

--- Commands/Esso.cs
+++ Commands/Esso.cs
@@ -31,41 +31,5 @@ namespace unbis_discord_bot.Commands
             }
             await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": " + target.Mention + " ist " + res + "% Normie").ConfigureAwait(false);
         }
-
-        [Command("esso")]
-        [Description("Zeit seit letzter Nachricht (in Sekunden) von Esso / Botneustart")]
-        public async Task LastEssoMsg(CommandContext ctx)
-        {
-            double result = 0;
-            DateTime current = DateTime.Now;
-            if (Bot.lastEssoMessage != null)
-            {
-                result = (current - Bot.lastEssoMessage.Timestamp).TotalSeconds;
-            }
-            else
-            {
-                result = (current - Bot.botStart).TotalSeconds;
-            }
-            await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": " + result + " Sekunden seit Esso etwas schrieb oder der Bot neugestartet wurde.").ConfigureAwait(false);
-        }
-
-        [Command("Ekr")]
-        [Description("Zeit seit letzter Nachricht (in Sekunden) von Esso / Botneustart")]
-        public async Task LastEkrMsg(CommandContext ctx)
-        {
-            double result = 0;
-            Bot.lastRattanMessageDt = new DateTime(2021, 3, 20, 4, 12, 0);
-            DateTime current = DateTime.Now;
-            if (Bot.lastRattanMessage != null)
-            {
-                result = (current - Bot.lastEssoMessage.Timestamp).TotalSeconds;
-            }
-            else
-            {
-                result = (current - Bot.lastRattanMessageDt).TotalSeconds;
-            }
-            
-            await ctx.Channel.SendMessageAsync(ctx.Member.Mention + ": " + result + " Sekunden seit Rattan etwas schrieb").ConfigureAwait(false);
-        }
     }
 }
